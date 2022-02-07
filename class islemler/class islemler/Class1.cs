1c2e49ab4bceafd27174cabe8ae1b5718e4a6bf3
Eei0 <94996237+Eei0@users.noreply.github.com>
2022-02-07 21:15:30 +0300

    Add files via upload

---
+++ class islemler/class islemler/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_islemler
+{
+    class İSLEMLER
+    {
+        public int topla(int a, int b)
+        {
+
+            return a+b;
+        }
+        public int cikar(int a, int b)
+        {
+
+            return a - b;
+        }
+        public int carp(int a, int b)
+        {
+
+            return a * b;
+        }
+        public int bol(int a, int b)
+        {
+
+            return a / b;
+        }
+
+    }
+}
---
+++ class islemler/class islemler/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace class_islemler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            İSLEMLER isl = new İSLEMLER();
+            MessageBox.Show(isl.topla(Convert.ToInt16(textBox1.Text),Convert.ToInt16(textBox2.Text)).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            İSLEMLER isl = new İSLEMLER();
+            MessageBox.Show(isl.cikar(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text)).ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            İSLEMLER isl = new İSLEMLER();
+            MessageBox.Show(isl.carp(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text)).ToString());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            İSLEMLER isl = new İSLEMLER();
+            MessageBox.Show(isl.bol(Convert.ToInt16(textBox1.Text), Convert.ToInt16(textBox2.Text)).ToString());
+        }
+    }
+}
